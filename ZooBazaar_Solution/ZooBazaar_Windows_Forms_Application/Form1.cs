5dfd7ac3d9ccd6c443d0907010a1f64387f2357d
Grzegorz Malisz <g.malisz@student.fontys.nl>
2022-09-29 10:48:50 +0200

    Grzegorz, Jesper - Begining of UI

--- ZooBazaar_Solution/ZooBazaar_Windows_Forms_Application/Form1.cs
+++ ZooBazaar_Solution/ZooBazaar_Windows_Forms_Application/Form1.cs
@@ -1,10 +1,21 @@
+using ZooBazaar_Windows_Forms_Application.controls;
+
 namespace ZooBazaar_Windows_Forms_Application
 {
     public partial class Form1 : Form
     {
+        private MainMenuTable mainMenutable;
+
         public Form1()
         {
             InitializeComponent();
+            mainMenutable = new MainMenuTable();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Controls.Add(mainMenutable);
         }
     }
 }
---
+++ ZooBazaar_Solution/ZooBazaar_Windows_Forms_Application/controls/MainMenuTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooBazaar_Windows_Forms_Application.controls
+{
+    internal class MainMenuTable : TableLayoutPanel
+    {
+        //Controls
+
+        //Color
+        SolidBrush highlightBrush;
+
+        public MainMenuTable()
+        {
+            //Controls
+
+
+            //Properties
+            Dock = DockStyle.Fill;
+            ColumnCount = 3;
+            ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100));
+            ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 10));
+            ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+
+
+            RowCount = 2;
+            RowStyles.Add(new RowStyle(SizeType.Absolute, 50));
+            RowStyles.Add(new RowStyle(SizeType.Percent, 100));
+
+
+
+            //Colors
+            highlightBrush = new SolidBrush(Color.Green);
+
+            //Events
+            this.CellPaint += TableLayoutPanel_CellPaint;
+        }
+
+
+        private void TableLayoutPanel_CellPaint(object? sender, TableLayoutCellPaintEventArgs e)
+        {
+            if (e.Row == 0 && e.Column == 2)
+            {
+                e.Graphics.FillRectangle(highlightBrush, e.CellBounds);
+            }
+
+
+            if (e.Column == 1)
+            {
+                e.Graphics.FillRectangle(highlightBrush, e.CellBounds);
+            }
+        }
+
+    }
+}
