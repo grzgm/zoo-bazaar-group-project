5bd42e241f9cb22572179ad53e711ed5b5a708b9
Jesper <jesper.s.js@gmail.com>
2022-10-02 18:46:00 +0200

    Jesper - updated interfaces

--- ZooBazaar_Solution/ZooBazaar_ClassLibrary/Interfaces/IAnimalMenager.cs
+++ ZooBazaar_Solution/ZooBazaar_ClassLibrary/Interfaces/IAnimalMenager.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZooBazaar_DomainModels.Models;
+
 
 namespace ZooBazaar_ClassLibrary.Interfaces
 {
     public interface IAnimalMenager
     {
+        Animal GetAnimal(int id);
+
+        List<Animal> GetAll();
+
+        Animal NewAnimal(int id, string name, int age, DateOnly dateOfBirth, bool sex, string species, string speciesType, string diet, int feedingtimeId, int feedingInterval, int zoneId, int habitatId);
+
+        Animal RemoveAnimal(int id);
+
+        Animal UpdateAnimal(); //WHAT THE FUCK TO I PASS? A FUCKING AnimalDTO OR RA DATA?
     }
 }
