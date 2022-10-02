bb4330a69c57a9486c9e20ec0eb1768adb450f7e
Jesper <jesper.s.js@gmail.com>
2022-10-02 19:17:14 +0200

    Jesper - Updated interfaces

--- ZooBazaar_Solution/ZooBazaar_ClassLibrary/Interfaces/IZoneMenager.cs
+++ ZooBazaar_Solution/ZooBazaar_ClassLibrary/Interfaces/IZoneMenager.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZooBazaar_DomainModels.Models;
+using ZooBazaar_DTO.DTOs;
 
 namespace ZooBazaar_ClassLibrary.Interfaces
 {
     public interface IZoneMenager
     {
+        Zone NewZone(ZoneDTO zoneDTO);
+
+        Zone RemoveZone(int id);
     }
 }
