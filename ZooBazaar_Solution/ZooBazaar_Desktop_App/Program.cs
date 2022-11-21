296733bbdcf141ba008542aa357b558bf1524a97
Michal Raczkowski <m.raczkowski@student.fontys.nl>
2022-11-21 12:54:10 +0100

    Addd electron desktop app

---
+++ ZooBazaar_Solution/ZooBazaar_Desktop_App/Program.cs
@@ -0,0 +1,40 @@
+using ElectronNET.API;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddElectron();
+builder.WebHost.UseElectron(args);
+if (HybridSupport.IsElectronActive)
+{
+    var window = await Electron.WindowManager.CreateWindowAsync();
+    window.OnClosed += () =>
+    {
+        Electron.App.Quit();
+    };
+}
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+Task.Run(async () => await Electron.WindowManager.CreateWindowAsync());
